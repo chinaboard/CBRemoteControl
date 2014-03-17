9e39a4bd75b78632e88393281f0824d93ac47a12
chinaboard <chinaboard@gmail.com>
2014-03-18 01:08:15 +0800

    修正服务器端内存泄露

--- CBRemoteControl.Server/Main.cs
+++ CBRemoteControl.Server/Main.cs
@@ -18,62 +18,56 @@ namespace CBRemoteControl.Server
 {
     public partial class Main : Form
     {
-        private bool allowClose;
         public Main()
         {
             InitializeComponent();
             this.notifyIcon.Visible = true;
-            this.allowClose = false;
             this.btn_Start.Enabled = false;
             this.ShowInTaskbar = false;
             this.WindowState = FormWindowState.Minimized;
             this.Hide();
             Task.Factory.StartNew(() => ServerHeartBeat());
             Task.Factory.StartNew(() => UploadScreen());
-            
         }
 
         private void UploadScreen()
         {
-            Task.Factory.StartNew(() =>
+            while (true)
             {
-                while (true)
+                try
                 {
-                    try
+                    if (ServerCommon.Instance.IsOnline)
                     {
-                        if (ServerCommon.Instance.IsOnline)
-                        {
-                            var tmp = InputScreen.GetScreen();
-                            WCFService.Instance.Client.PutScreenCompleted += Client_PutScreenCompleted;
-                            WCFService.Instance.Client.PutScreenAsync(tmp);
-                            Thread.Sleep(ServerCommon.Instance.TimeSlice);
-                        }
-                        
+                        var tmp = InputScreen.GetScreen();
+                        WCFService.Instance.Client.PutScreen(tmp);
+                        Thread.Sleep(ServerCommon.Instance.TimeSlice);
                     }
-                    catch { }
+                    else
+                    {
+                        Thread.Sleep(10000);
+                    }
                 }
-            });
-        }
-
-        void Client_PutScreenCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            GC.Collect();
+                catch
+                {
+                    Thread.Sleep(30000);
+                }
+            }
         }
         private void ServerHeartBeat()
         {
-            Task.Factory.StartNew(() =>
+            while (true)
+            {
+                try
                 {
-                    while (true)
-                    {
-                        try
-                        {
-                            ServerCommon.Instance.SetOnline(WCFService.Instance.Client.HeartBeat(ServerCommon.Instance.CBServerInfo));
-                            this.Invoke(new Action(() => this.lblNowTime.Text = DateTime.Now.ToString()));
-                            Thread.Sleep(ServerCommon.Instance.HeartBeatTime);
-                        }
-                        catch { }
-                    }
-                });
+                    ServerCommon.Instance.SetOnline(WCFService.Instance.Client.HeartBeat(ServerCommon.Instance.CBServerInfo));
+                    this.Invoke(new Action(() => this.lblNowTime.Text = DateTime.Now.ToString()));
+                    Thread.Sleep(ServerCommon.Instance.HeartBeatTime);
+                }
+                catch
+                {
+                    Thread.Sleep(30000);
+                }
+            }
         }
         private void btn_Start_Click(object sender, EventArgs e)
         {
